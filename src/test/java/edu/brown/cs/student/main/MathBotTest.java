524d51b7c595dd39f96bbcec9c57d44f1e714eef
dtsryBKJnugY3v2v <dtsryBKJnugY3v2v@gmail.com>
2021-09-14 16:06:31 -0400

    added JUnit tests

--- src/test/java/edu/brown/cs/student/main/MathBotTest.java
+++ src/test/java/edu/brown/cs/student/main/MathBotTest.java
@@ -27,5 +27,37 @@ public class MathBotTest {
     assertEquals(1, output, 0.01);
   }
 
+  @Test
+  public void testLargerSubtraction() {
+    MathBot mb = new MathBot();
+    double output = mb.subtract(21423634, 231245);
+    assertEquals(21192389, output, 0.01);
+  }
+
+  @Test
+  public void testZeros() {
+    MathBot mb = new MathBot();
+    double output = mb.add(0, 2);
+    assertEquals(2, output, 0.01);
+    double output2 = mb.subtract(4, 0);
+    assertEquals(4, output, 0.01);
+    double output3 = mb.add(0,0);
+    assertEquals(0,output3, 0.01);
+  }
+
+  @Test
+  public void testNegatives() {
+    MathBot mb = new MathBot();
+    double output = mb.subtract(3,6);
+    assertEquals(-3, output, 0.01);
+  }
+
+  @Test
+  public void testSmallerNumbers() {
+    MathBot mb = new MathBot();
+    double output = mb.add(0.1, 0.1);
+    assertEquals(0.2, output, 0.001);
+  }
+
   // TODO: add more unit tests of your own
 }
